61dc5ba3a8e9a6c22c9f7c5b30ecfbe543dbfb91
Loverveysel <alpercanzer_1600@hotmail.com>
2024-03-07 22:46:56 +0300

    deploy

--- src/lib/navigate.ts
+++ src/lib/navigate.ts
@@ -1,7 +1,11 @@
-'use server'
- 
-import { redirect } from 'next/navigation'
- 
-export  default async function navigate(path : string) {
-  redirect(path)
+import { useRouter } from 'next/router'
+
+export default function navigate(path: string) {
+  const router = useRouter()
+
+  const navigator = async () => {
+    await router.push(path)
+  }
+
+  return navigator
 }
