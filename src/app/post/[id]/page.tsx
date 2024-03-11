a4df75eb5be087ac9b8f884459f2a1417e50ad47
Loverveysel <alpercanzer_1600@hotmail.com>
2024-03-11 17:44:34 +0300

    deploy

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -59,7 +59,11 @@ export default function Page({ params }: { params: { id: string } }) {
       )
   } else{
     return (
-        <div className='bg-base-100 text-black font-semibold text-2xl m-auto items-center justify-center '>Idea not found...</div>
+        <div className='bg-base-100 w-screen h-screen'>
+            <div className='absolute flex top-1/2 right-1/2 translate-x-1/2 translate-y-1/2'>
+                <img className='rounded-full animate-ping m-auto border-4 border-white' src="/icon.ico" alt="" height={150} width={150} />
+            </div>
+        </div>
     )
   }
 }
